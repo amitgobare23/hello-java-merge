aaeecfe1c034fd4a1df79df79f1497c42e2f6aa9
Amit Gobare <amitgobare23@gmail.com>
2022-09-28 19:23:13 +0530

    after unstashed changes

--- hello-java-merge/src/helloPackage/HelloTest.java
+++ hello-java-merge/src/helloPackage/HelloTest.java
@@ -7,6 +7,7 @@ public class HelloTest {
 		System.out.println("Hello from local repository again");
 		System.out.println("Hello from Remote repository");
 		System.out.println("this line is added after stash changes");
+		System.out.println("Hello from local repository one more time");
 	}
 
 }
